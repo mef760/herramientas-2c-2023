9b16309bcebfe7813712fb7ddee02eb74fc17df8
Manuel Fondovila <mfondovila@riu.com>
2023-08-29 21:29:41 -0300

    first commit

---
+++ clase1/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace clase1.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public List<string> Profesiones { get; set; }
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+        var list = new List<string>();
+
+        list.Add("Vendedor");
+        list.Add("Analista funcional");
+        list.Add("Analista QA");
+        list.Add("Profesor");
+        list.Add("Politico");
+
+        Profesiones = list;
+    }
+}
---
+++ clase2/Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace clase2.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    [DisplayName("Nombre de usuario")]
+    [Required]
+    public string UserName { get; set; }
+    [DisplayName("Correo electronico")]
+    [Required]
+    public string Mail { get; set; }
+    [Range(18, 99)]
+    public int Age { get; set; }
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        UserName = "usuario nuevo";
+    }
+
+    public void OnPost(IndexModel obj){
+        if(!ModelState.IsValid)
+        {
+           // logica de validacion
+        }
+
+        var name = obj.UserName;
+        var mail = obj.Mail;
+    }
+}
